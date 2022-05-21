fc2b657f3bd3f24cbc48f913863acf714fb2097c
Slazer22 <48864818+Slazer22@users.noreply.github.com>
2022-05-21 00:28:42 -0500

    Add project files.

---
+++ Attendance Roster/Program.cs
@@ -0,0 +1,26 @@
+﻿/*This program will create an attendance roster for all the students in this C# Course*/
+
+using System;
+
+namespace Students
+{
+
+    class Roster
+    {
+        public static void Main(string[] args)
+        {
+
+            string[] students = { "Mark", "Christopher", "Christina", "John", "Avery", "Joe", "Phillip" };
+
+
+            Console.WriteLine("Please print out a list of students in this C# Course");
+            Console.ReadKey();
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i]);
+            }
+        }
+
+    }
+}
